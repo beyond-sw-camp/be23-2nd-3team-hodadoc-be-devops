db7c7f27167c46b8a8a8c7ce7a7a79a6ec58f3b8
junhyeong <qjhyng00@naver.com>
2026-03-16 17:51:48 +0900

    fix11

--- src/main/java/com/beyond/hodadoc/chat/config/StompWebSocketConfig.java
+++ src/main/java/com/beyond/hodadoc/chat/config/StompWebSocketConfig.java
@@ -20,7 +20,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/connect")
-                .setAllowedOriginPatterns("http://localhost:*", "http://192.168.*:*", "https://www.hodadoc.com")
+                .setAllowedOriginPatterns("http://localhost:*", "http://192.168.*:*", "https://www.hodadoc.littleniddle.store", "https://www.hodadoc.admin.littleniddle.store")
 //                ws://가 아닌 http:// 엔드포인트를 사용할 수 있게 해주는 sockJs라이브러리를 통한 요청을 허용하는 설정.
                 .withSockJS();
 
