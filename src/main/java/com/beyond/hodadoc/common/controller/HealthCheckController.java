ce3b24833e126e5bf0c6ce6b4d0c16407477befb
junhyeong <qjhyng00@naver.com>
2026-03-16 16:16:32 +0900

    fix7

--- src/main/java/com/beyond/hodadoc/common/controller/HealthCheckController.java
+++ src/main/java/com/beyond/hodadoc/common/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.example.ordersystem.common.controller;
+package com.beyond.hodadoc.common.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
